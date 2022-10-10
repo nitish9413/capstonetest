a34bce4c944bcd73c383ba64a9807fd13e5d6847
nitish9413 <nitishkatkade94@gmail.com>
2022-10-10 14:37:51 +0530

    f

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from io import StringIO
 from pathlib import Path
 import streamlit as st
 import time
-from detect import detect
+import detect
 import os
 import sys
-import argparse
+# import argparse
 from PIL import Image
 
 
@@ -30,45 +30,45 @@ def get_detection_folder():
 
 if __name__ == '__main__':
 
-    st.title('YOLOv5 Streamlit App')
+    st.title('YOLOv5 Weapon Detection App')
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str,
-                        default='best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str,
-                        default='data/images', help='source')
-    parser.add_argument('--img-size', type=int, default=640,
-                        help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.35, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true',
-                        help='display results')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true',
-                        help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true',
-                        help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int,
-                        help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument('--update', action='store_true',
-                        help='update all models')
-    parser.add_argument('--project', default='runs/detect',
-                        help='save results to project/name')
-    parser.add_argument('--name', default='exp',
-                        help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true',
-                        help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    print(opt)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--weights', nargs='+', type=str,
+    #                     default='best.pt', help='model.pt path(s)')
+    # parser.add_argument('--source', type=str,
+    #                     default='data/images', help='source')
+    # parser.add_argument('--img-size', type=int, default=640,
+    #                     help='inference size (pixels)')
+    # parser.add_argument('--conf-thres', type=float,
+    #                     default=0.35, help='object confidence threshold')
+    # parser.add_argument('--iou-thres', type=float,
+    #                     default=0.45, help='IOU threshold for NMS')
+    # parser.add_argument('--device', default='',
+    #                     help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    # parser.add_argument('--view-img', action='store_true',
+    #                     help='display results')
+    # parser.add_argument('--save-txt', action='store_true',
+    #                     help='save results to *.txt')
+    # parser.add_argument('--save-conf', action='store_true',
+    #                     help='save confidences in --save-txt labels')
+    # parser.add_argument('--nosave', action='store_true',
+    #                     help='do not save images/videos')
+    # parser.add_argument('--classes', nargs='+', type=int,
+    #                     help='filter by class: --class 0, or --class 0 2 3')
+    # parser.add_argument('--agnostic-nms', action='store_true',
+    #                     help='class-agnostic NMS')
+    # parser.add_argument('--augment', action='store_true',
+    #                     help='augmented inference')
+    # parser.add_argument('--update', action='store_true',
+    #                     help='update all models')
+    # parser.add_argument('--project', default='runs/detect',
+    #                     help='save results to project/name')
+    # parser.add_argument('--name', default='exp',
+    #                     help='save results to project/name')
+    # parser.add_argument('--exist-ok', action='store_true',
+    #                     help='existing project/name ok, do not increment')
+    # opt = parser.parse_args()
+    # print(opt)
 
     source = ("Image detection", "Video detection")
     source_index = st.sidebar.selectbox("select input", range(
@@ -83,7 +83,7 @@ if __name__ == '__main__':
                 st.sidebar.image(uploaded_file)
                 picture = Image.open(uploaded_file)
                 picture = picture.save(f'data/images/{uploaded_file.name}')
-                opt.source = f'data/images/{uploaded_file.name}'
+                source = f'data/images/{uploaded_file.name}'
         else:
             is_valid = False
     else:
@@ -94,7 +94,7 @@ if __name__ == '__main__':
                 st.sidebar.video(uploaded_file)
                 with open(os.path.join("data", "videos", uploaded_file.name), "wb") as f:
                     f.write(uploaded_file.getbuffer())
-                opt.source = f'data/videos/{uploaded_file.name}'
+                source = f'data/videos/{uploaded_file.name}'
         else:
             is_valid = False
 
@@ -102,17 +102,19 @@ if __name__ == '__main__':
         print('valid')
         if st.button('start detection'):
 
-            detect(opt)
+            detect.run(weights='best.pt', source=source)
 
             if source_index == 0:
                 with st.spinner(text='Preparing Images'):
                     for img in os.listdir(get_detection_folder()):
                         st.image(str(Path(f'{get_detection_folder()}') / img))
 
-                    st.balloons()
+                    #st.balloons()
             else:
                 with st.spinner(text='Preparing Video'):
                     for vid in os.listdir(get_detection_folder()):
-                        st.video(str(Path(f'{get_detection_folder()}') / vid))
+                        video_file=open(str(Path(f'{get_detection_folder()}') / vid), 'rb')
+                        video_bytes = video_file.read()
+                        st.video(video_bytes) #str(Path(f'{get_detection_folder()}') / vid)
 
-                    st.balloons()
+                    #st.balloons()
